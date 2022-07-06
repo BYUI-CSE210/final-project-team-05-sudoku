59b395c3be27861d8024066e70218c3c93d8a3f8
mictlantecutli <moisesparra82@hotmail.com>
2022-07-06 02:35:58 -0500

    intent to draw board

--- sudoku/__main__.py
+++ sudoku/__main__.py
@@ -3,7 +3,7 @@ import random
 
 from tkinter.tix import CELL
 
-from raylib import FONT_BITMAP
+from raylib import DEFAULT, FONT_BITMAP
 
 from game.casting.actor import Actor
 from game.casting.artifact import Artifact
@@ -31,10 +31,23 @@ FONT_SIZE = 50
 
 COLS = 9
 ROWS = 9
-CAPTION = "Robot Finds Kitten"
+CAPTION = "Sudoku Game"
 DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
 WHITE = Color(255, 255, 255)
-DEFAULT_ARTIFACTS = 40
+#DEFAULT_ARTIFACTS = 40
+MATRIX = 9
+
+INITIAL_BOARD = [
+    [7, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 3, 0, 4, 0, 0, 0, 2],
+    [1, 0, 8, 5, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 9, 5, 6, 4],
+    [0, 0, 0, 6, 0, 8, 0, 0, 0],
+    [6, 9, 7, 4, 0, 0, 8, 0, 0],
+    [0, 0, 0, 0, 0, 2, 6, 0, 9],
+    [3, 0, 0, 0, 7, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 5],
+]
 
 
 def main():
@@ -63,31 +76,35 @@ def main():
     cast.add_actor("robots", robot)
 
     # create the artifacts
-    with open(DATA_PATH) as file:
-        data = file.read()
-        messages = data.splitlines()
+    # with open(DATA_PATH) as file:
+    #   data = file.read()
+    #  messages = data.splitlines()
 
-    for n in range(DEFAULT_ARTIFACTS):
-        text = chr(random.randint(33, 126))
-        message = messages[n]
+    for n in range(MATRIX):
+        for j in range(MATRIX):
+            if INITIAL_BOARD[n][j] != 0:
+                text = str(INITIAL_BOARD[n][j])
 
-        x = random.randint(1, COLS - 1)
-        y = random.randint(1, ROWS - 1)
-        position = Point(x, y)
-        position = position.scale(CELL_SIZE)
+    #text = chr(random.randint(33, 126))
+    #message = messages[n]
 
-        r = random.randint(0, 255)
-        g = random.randint(0, 255)
-        b = random.randint(0, 255)
-        color = Color(r, g, b)
+                #x = random.randint(1, COLS - 1)
+                #y = random.randint(1, ROWS - 1)
+                position = Point(n, j)
+                position = position.scale(CELL_SIZE)
 
-        artifact = Artifact()
-        artifact.set_text(text)
-        artifact.set_font_size(FONT_SIZE)
-        artifact.set_color(color)
-        artifact.set_position(position)
-        artifact.set_message(message)
-        cast.add_actor("artifacts", artifact)
+                r = random.randint(0, 255)
+                g = random.randint(0, 255)
+                b = random.randint(0, 255)
+                color = Color(r, g, b)
+
+                artifact = Artifact()
+                artifact.set_text(text)
+                artifact.set_font_size(FONT_SIZE)
+                artifact.set_color(color)
+                artifact.set_position(position)
+                # artifact.set_message(message)
+                cast.add_actor("artifacts", artifact)
 
     # start the game
     keyboard_service = KeyboardService(CELL_SIZE)
